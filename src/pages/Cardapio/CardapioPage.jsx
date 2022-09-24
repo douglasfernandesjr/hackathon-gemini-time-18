241f2191e608abb8549aa063b7fd755f55853792
Juliany <juliany12121989@gmail.com>
2022-09-24 12:29:23 -0300

    inicial tela de detalhes restaurante

---
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -0,0 +1,12 @@
+import { Container, Typography, CircularProgress, Grid } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { getDetalhes } from "../../services/detalhes.service";
+import {useParams} from "react-router-dom"
+import "./style.css";
+
+function CardapioPage() {
+  
+    
+}
+
+export default CardapioPage;
