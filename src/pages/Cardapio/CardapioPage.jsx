2796c627e142dd2ee541e61419ab22f384b8bf60
PedroBitencourt <pedro.bitencourt42@gmail.com>
2022-09-24 13:03:07 -0300

    Chamada do detalhes service

--- src/pages/Cardapio/CardapioPage.jsx
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -2,23 +2,32 @@ import { Container, Typography, CircularProgress, Grid } from "@material-ui/core
 import { useEffect, useState } from "react";
 import { getDetalhes } from "../../services/detalhes.service";
 import {useParams} from "react-router-dom"
-import "./style.css";
+import "./styles.css";
 
 function CardapioPage() {
-    const [nomeCategoria, setNomeCategoria] = useState([]);
-    const [restaurantesBaratinho, setRestaurantesBaratinho] = useState();
-    const [restaurantesNoPreco, setRestaurantesNoPreco] = useState([]);
-    const [restaurantesCaro, setRestaurantesCaro] = useState([]);
+    
     const [loading, setLoading] = useState(true);
   
-    const {id} = useParams();
-  
+    const [cardapio, setCardapio] = useState();
+    const [nomeCategoria, setNomeCategoria] = useState([]);
+    const [item, setItem] = useState([]);
+    const [nomeRestaurante, setNomeRestaurante] = useState();
+    const [tempoMedio, setTempoMedio] = useState();
+    const [valorEntrega, setValorEntrega] = useState();
+    const [nota, setNota] = useState();
+    const id = useParams();
     useEffect(() => {
         getDetalhes(id).then((response) => {
-            
-      })
-    }, []);
-
+          setCardapio(response.cardapio);  
+          setNomeCategoria(response.cardapio.categoria)
+          setItem(response.cardapio.categoria.itens);
+          setNomeRestaurante(response.nome);
+          setNota(response.nota);
+          setTempoMedio(response.tempo_medio);
+          setValorEntrega(response.valor_entrega);
+          console.log(id)
+        })
+      }, []);
 }
 
 export default CardapioPage;
