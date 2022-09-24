4df05662f6e552da8ff7ab5f0e611786b02eaac4
Juliany <juliany12121989@gmail.com>
2022-09-24 14:33:56 -0300

    layout inicial

--- src/pages/Cardapio/CardapioPage.jsx
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -1,33 +1,53 @@
 import { Container, Typography, CircularProgress, Grid } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { getDetalhes } from "../../services/detalhes.service";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import "./styles.css";
 
 function CardapioPage() {
-    
+
     const [loading, setLoading] = useState(true);
-  
-    const [cardapio, setCardapio] = useState();
-    const [nomeCategoria, setNomeCategoria] = useState([]);
-    const [item, setItem] = useState([]);
+
     const [nomeRestaurante, setNomeRestaurante] = useState();
+    const [imagemRestaurante, setImagemNomeRestaurante] = useState();
+    const [distanciaRestaurante, setDistanciaRestaurante] = useState();
     const [tempoMedio, setTempoMedio] = useState();
     const [valorEntrega, setValorEntrega] = useState();
     const [nota, setNota] = useState();
-    const id = useParams();
+    const [endereco, setEndereco] = useState();
+    const [descricao, setDescricao] = useState();
+
+    const { id } = useParams();
+
     useEffect(() => {
         getDetalhes(id).then((response) => {
-          setCardapio(response.cardapio);  
-          setNomeCategoria(response.cardapio.categoria)
-          setItem(response.cardapio.categoria.itens);
-          setNomeRestaurante(response.nome);
-          setNota(response.nota);
-          setTempoMedio(response.tempo_medio);
-          setValorEntrega(response.valor_entrega);
-          console.log(id)
+            setNomeRestaurante(response.nome);
+            setImagemNomeRestaurante(response.imagem)
+            setDistanciaRestaurante(response.distancia)
+            setNota(response.nota);
+            setTempoMedio(response.tempo_medio);
+            setValorEntrega(response.valor_entrega);
+            setEndereco(response.endereco);
+            setDescricao(response.descricao)
         })
-      }, []);
+    }, []);
+
+    return (
+        <div className="cardRestaurante">
+            <div className="img-detalhes">
+                <img class="imgRestaurante" src={imagemRestaurante} />
+                <div className="detalhesRestaurante">
+                    <h4>{nomeRestaurante}</h4>
+                    <p>{distanciaRestaurante}</p>
+                    <p className="nota">{nota}</p>
+                    <p>{tempoMedio}</p>
+                    <p>{valorEntrega}</p>
+                </div>
+            </div>
+            <p>{descricao}</p>
+            <p>{endereco}</p>
+        </div>
+    )
 }
 
 export default CardapioPage;
